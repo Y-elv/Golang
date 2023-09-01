3a7bda4132abb4655f9f98c8b1b3a24d1518287b
Y-elv <mugishaelvis456@gmail.com>
2023-09-01 12:08:24 +0200

    finish up arrays with slice

--- main.go
+++ main.go
@@ -17,5 +17,34 @@ func main() {
 	fmt.Printf("you scored %f points!\n", score)
 	var str = fmt.Sprintf("my name is %v and my age is %v \n", name, age)
 	println(str)
+	var marks = [3]int{10, 11, 23}
+	fmt.Println(marks, len(marks))
+	fmt.Println(len(marks))
+
+	fmt.Println("after modification")
+	marks[1] = 12
+	fmt.Println(marks, len(marks))
+
+	names := [2]string{"hey", "you"}
+	fmt.Println(names)
+
+	//slices
+
+	var scores = []int{1, 2, 3}
+	scores[2] = 4
+
+	scores = append(scores, 35)
+	fmt.Println(scores, len(scores))
+
+	//slice range
+
+	rangeOne := scores[1:3]
+	fmt.Println(rangeOne)
+	rangeTwo := scores[1:]
+	fmt.Println(rangeTwo)
+	rangeThree := scores[:3]
+	fmt.Println(rangeThree)
+	rangeThree = append(rangeThree, 100)
+	fmt.Println(rangeThree)
 
 }
