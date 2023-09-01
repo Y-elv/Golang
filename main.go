e5d3afef951d8f17fd278a6a908540135d95396a
Y-elv <mugishaelvis456@gmail.com>
2023-09-01 10:41:35 +0200

    continuation on studies of variables

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello elvis")
+
+	var name string = "elvis"
+	secondName := "angel"
+	var age int = 0
+	var age1 uint = 2
+
+	fmt.Println("hello", name, "your age is:", age)
+	fmt.Print(secondName, age1, "\n")
+	fmt.Printf("my name is %v and my age is %v", name, age)
 }
