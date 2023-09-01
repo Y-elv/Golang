ea3e1ba6a028af0e6f48ec843f30463582113827
Y-elv <mugishaelvis456@gmail.com>
2023-09-01 14:33:54 +0200

    finish up loops

--- loops.go
+++ loops.go
@@ -14,12 +14,22 @@ func main() {
 	for i := 0; i < 5; i++ {
 		fmt.Println("the value of i is :", i)
 	}
-
+	fmt.Println("\n")
 	names := []string{"kizox", "rugira", "eddy", "rufido"}
 	days := []string{"st", "nd", "rd", "th"}
 
 	for i := 0; i < len(names); i++ {
 		formatted := fmt.Sprintf("the %v %v name is %v\n", i+1, days[i], names[i])
 		fmt.Print(formatted)
+	}
+
+	fmt.Println("\n")
+	for index, value := range names {
+		fmt.Printf("the value at index %v is %v\n", index, value)
+	}
+	//for ommiting one of this two (index,value)
+	fmt.Println("\n")
+	for _, value := range names {
+		fmt.Printf("the value is %v\n", value)
 	}
 }
