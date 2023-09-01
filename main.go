ed9a5c871ba5be7383ebb9dbac296f5f081d204a
Y-elv <mugishaelvis456@gmail.com>
2023-09-01 10:59:37 +0200

    arrays and slice

--- main.go
+++ main.go
@@ -8,8 +8,14 @@ func main() {
 	secondName := "angel"
 	var age int = 0
 	var age1 uint = 2
+	score := 30.2
 
 	fmt.Println("hello", name, "your age is:", age)
 	fmt.Print(secondName, age1, "\n")
-	fmt.Printf("my name is %v and my age is %v", name, age)
+	fmt.Printf("my name is %v and my age is %v \n", name, age)
+	fmt.Printf("the type of this age is %T \n", age)
+	fmt.Printf("you scored %f points!\n", score)
+	var str = fmt.Sprintf("my name is %v and my age is %v \n", name, age)
+	println(str)
+
 }
