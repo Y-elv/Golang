410ad2c80c25b16b7f80f64afbeef72b94a08504
mugisha <mugishaelvis456@gmail.com>
2023-09-02 18:49:22 +0200

    map

--- map.go
+++ map.go
@@ -1,7 +1,29 @@
-package main 
+package main
 
 import "fmt"
 
-func main (){
-	fmt.Println("ello")
+func main() {
+	menu := map[string]float64{
+		"soup":          4.99,
+		"pie":           7.99,
+		"salad":         6.99,
+		"toffe pudding": 3.55,
+	}
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	//looping
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	phoneNumber := map[int]string{
+		37888: "elvis",
+		56557: "eddy",
+		44444: "anel",
+	}
+	fmt.Println(phoneNumber)
+	fmt.Println(phoneNumber[44444])
+	phoneNumber[44444] = "ian"
+	fmt.Println(phoneNumber)
 }
